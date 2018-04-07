dbed0456ea22e1bea12b7d8853a2f464df9153ba
Scott Vandehey <fake-scott@spaceninja.com>
2018-04-07 16:00:46 -0700

    improve sorting

--- js/listbooks.js
+++ js/listbooks.js
@@ -8,18 +8,31 @@ const xhr = new XMLHttpRequest();
 let books = [];
 
 //
+// Parse the Book Data
+//
+const parseBooks = function parseBooks(input) {
+  const bookArray = input;
+
+  for (let i = 0; i < bookArray.length; i += 1) {
+    bookArray[i].rating = bookArray[i].rating.toFixed(1);
+  }
+
+  return bookArray;
+};
+
+//
 // Sort an array by keys
 //
-const sortByKeys = function sortByKeys(array, key1, key2, sortOrder) {
-  let s = '';
-
-  if (sortOrder === 'descending') {
-    s = firstBy(key1, { ignoreCase: true, direction: -1 })
-      .thenBy(key2, { ignoreCase: true, direction: -1 });
-  } else {
-    s = firstBy(key1, { ignoreCase: true })
-      .thenBy(key2, { ignoreCase: true });
-  }
+const sortByKeys = function sortByKeys(array, key1, key1Sort, key2, key2Sort) {
+  const s1 = {
+    ignoreCase: true,
+    direction: key1Sort === 'descending' ? -1 : 0,
+  };
+  const s2 = {
+    ignoreCase: true,
+    direction: key2Sort === 'descending' ? -1 : 0,
+  };
+  const s = firstBy(key1, s1).thenBy(key2, s2);
 
   return array.sort(s);
 };
@@ -39,14 +52,13 @@ const buildHTML = function buildHTML(bookArray) {
       t.querySelector('.book--rating').innerHTML = bookArray[i].rating;
       t.querySelector('.book--length').innerHTML = bookArray[i].length;
       if (bookArray[i].series) {
-        t.querySelector('.book--series').innerHTML =
-          `${bookArray[i].series.number}/${bookArray[i].series.length}`;
+        t.querySelector('.book--series').innerHTML = bookArray[i].series;
       }
 
       booklistContent.appendChild(t);
     }
   } else {
-    const error = 'Oh no! Your browser doesn\'t support template!';
+    const error = 'Oh no! Your browser doesn\'t support templates!';
     booklistError.innerHTML = error;
   }
 };
@@ -60,8 +72,9 @@ const sortBy = function sortBy() {
   sortByKeys(
     books,
     sortOption.value,
-    sortOption.dataset.sortSecondary,
     sortOption.dataset.sortOrder,
+    sortOption.dataset.sortSecondary,
+    sortOption.dataset.sortSecondaryOrder,
   );
 
   buildHTML(books);
@@ -79,7 +92,7 @@ xhr.open('GET', 'data/books.json', true);
 
 xhr.onload = function ajaxOnLoad() {
   if (xhr.status >= 200 && xhr.status < 400) {
-    books = JSON.parse(xhr.responseText);
+    books = parseBooks(JSON.parse(xhr.responseText));
     booklistSort.dispatchEvent(new Event('change'));
   } else {
     const error = 'Whoops! Something went wrong. Please try again.';
